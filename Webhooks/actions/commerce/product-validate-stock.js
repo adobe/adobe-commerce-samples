d4ad42fd64b2871dcb34c1758e5a38a948abcadb
Gabriel Ranghetti <gabriel.ranghetti@toyota.com>
2024-05-23 18:26:22 -0300

    Added webhooks code sample app.

---
+++ Webhooks/actions/commerce/product-validate-stock.js
@@ -0,0 +1,68 @@
+/*
+* <license header>
+*/
+
+/**
+ * This is a sample action showcasing how to handle Adobe Commerce add to cart webhook.
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main(params) {
+    // create a Logger
+    const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+    try {
+        // 'info' is the default level if not set
+        logger.info('Calling action product-validate-stock')
+
+        // log parameters, only if params.LOG_LEVEL === 'debug'
+        logger.debug(stringParameters(params))
+
+        // check for missing request input parameters and headers
+        const requiredParams = ['product', 'info']
+        const errorMessage = checkMissingRequestInputs(params, requiredParams)
+        if (errorMessage) {
+            // return and log client errors
+            return errorResponse(400, errorMessage, logger)
+        }
+
+        const info = params.info;
+        const product = params.product;
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                op: 'success'
+            })
+        };
+
+        if (!product.quantity_and_stock_status.is_in_stock || product.quantity_and_stock_status.qty < info.qty) {
+            response.body = JSON.stringify({
+                op: 'exception',
+                message: 'The product is out of stock.'
+            });
+        }
+
+        // log the response status code
+        logger.info(`${response.statusCode}: successful request`)
+
+        return response
+    } catch (error) {
+        // log any server errors
+        logger.error(error)
+        // return with 500
+        return errorResponse(500, 'server error', logger)
+    }
+}
+
+exports.main = main
