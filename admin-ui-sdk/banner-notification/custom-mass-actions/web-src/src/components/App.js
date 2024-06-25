ee84ed1ef36f5dd46e73f8000c8a8eec6f3d009a
Mariam Araujo Betancourt <maraujo@mariams-mbp-2.lan>
2024-06-25 02:17:10 -0700

    initial file structure for bannerNotification extension point

---
+++ admin-ui-sdk/banner-notification/custom-mass-actions/web-src/src/components/App.js
@@ -0,0 +1,61 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import React from 'react'
+import { Provider, lightTheme } from '@adobe/react-spectrum'
+import { ErrorBoundary } from 'react-error-boundary'
+import { Route, Routes, HashRouter } from 'react-router-dom'
+import ExtensionRegistration from './ExtensionRegistration'
+import { OrderMassAction } from './ProductMassAction'
+import { ProductMassAction } from './OrderMassAction'
+
+function App (props) {
+  // use exc runtime event handlers
+  // respond to configuration change events (e.g. user switches org)
+  props.runtime.on('configuration', ({ imsOrg, imsToken }) => {
+    console.log('configuration change', { imsOrg, imsToken })
+  })
+  // respond to history change events
+  props.runtime.on('history', ({ type, path }) => {
+    console.log('history change', { type, path })
+  })
+
+  return (
+      <ErrorBoundary onError={onError} FallbackComponent={fallbackComponent}>
+          <HashRouter>
+              <Provider theme={lightTheme} colorScheme={'light'}>
+                  <Routes>
+                      <Route index element={<ExtensionRegistration runtime={props.runtime} ims={props.ims} />} />
+                      <Route path={'order-mass-action'} element={<OrderMassAction runtime={props.runtime} ims={props.ims} />} />
+                      <Route path={'product-mass-action'} element={<ProductMassAction runtime={props.runtime} ims={props.ims} />} />
+                  </Routes>
+              </Provider>
+          </HashRouter>
+      </ErrorBoundary>
+  )
+
+  // Methods
+
+  // error handler on UI rendering failure
+    function onError(e, componentStack) {}
+
+    // component to show if UI fails rendering
+    function fallbackComponent({ componentStack, error }) {
+        return (
+            <React.Fragment>
+                <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Something went wrong :(</h1>
+                <pre>{componentStack + '\n' + error.message}</pre>
+            </React.Fragment>
+        )
+    }
+}
+
+export default App
---
+++ admin-ui-sdk/banner-notification/custom-mass-actions/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,59 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import { register } from '@adobe/uix-guest'
+
+export default function ExtensionRegistration() {
+    init().catch(console.error)
+}
+
+const init = async () => {
+    const orderExtensionId = 'order-custom-mass-action'
+    const productExtensionId = 'product-custom-mass-action'
+
+    await register({
+        id: orderExtensionId,
+        methods: {
+            bannerNotification: {
+                getMassActions() {
+                    return {
+                        order: [
+                            {
+                                actionId: `${orderExtensionId}::second-mass-action`,
+                                successMessage: 'Yay it works',
+                                errorMessage: 'BOO it failed'
+                            }
+                        ],
+                    }
+                }
+            }
+        }
+    })
+
+    await register({
+        id: productExtensionId,
+        methods: {
+            bannerNotification: {
+                getMassActions() {
+                    return {
+                        product: [
+                            {
+                                actionId: `${productExtensionId}::second-mass-action`,
+                                successMessage: 'Yay it works',
+                                errorMessage: 'BOO it failed'
+                            }
+                        ],
+                    }
+                }
+            }
+        }
+    })
+}
