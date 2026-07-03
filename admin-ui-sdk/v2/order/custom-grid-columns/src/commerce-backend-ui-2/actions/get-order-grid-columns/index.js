7c56cdf47364c41403b852fbb8f7019850d76d7b
Oriol Barcelona <obarcelonap@gmail.com>
2026-07-03 11:03:14 +0200

    CEXT-6409: add order/custom-grid-columns v2 sample (#58)
    * CEXT-6409: add v2 order/custom-grid-columns sample with app management
    
    * CEXT-6409: add README for v2 order/custom-grid-columns sample
    
    * CEXT-6409: use nodejs:24 everywhere
    
    * fix: align order v2 action path with product/customer pattern
    
    * fix: restore codegen comments in app.config.yaml and install.yaml
    
    * fix: update action file path in README

---
+++ admin-ui-sdk/v2/order/custom-grid-columns/src/commerce-backend-ui-2/actions/get-order-grid-columns/index.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { errorGridResponse, okGridResponse, parseGridRequest } from "@adobe/aio-commerce-sdk/admin-ui/grid-columns";
+
+const ORDER_COLUMNS = {
+    "000000001": { first_column: "value_1", second_column: 1, third_column: "2030-12-01T23:25:42+11:00" },
+    "000000002": { first_column: 1,         second_column: "test", third_column: "2011-10-02T23:25:42+00:00" },
+    "000000003": { first_column: "value_3", second_column: 3, third_column: "2030-14-01T23:25:42+11:00" },
+    "000000004": { first_column: "value_1", second_column: 1, third_column: "2011-10-02T23:25:42+00:00" },
+};
+
+const DEFAULTS = { first_column: "Default value first column", second_column: 0 };
+
+export async function main(params) {
+    try {
+        const { ids } = parseGridRequest(params);
+        const data = {};
+        for (const id of ids) {
+            if (ORDER_COLUMNS[id]) {
+                data[id] = ORDER_COLUMNS[id];
+            }
+        }
+        return okGridResponse(data, DEFAULTS);
+    } catch (e) {
+        return errorGridResponse(400, e.message);
+    }
+}
