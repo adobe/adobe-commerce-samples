9b03c4d2ff30bb84a350cc703d1d69e7219cb505
Jorge Cuerdo Alvarez <jcuerdo@adobe.com>
2026-07-06 12:44:30 +0200

    CEXT-6410: regenerate order custom-mass-action v2 with current migration tooling

---
+++ admin-ui-sdk/v2/order/custom-mass-action/src/commerce-backend-ui-2/web-src/src/pages/main-page.tsx
@@ -0,0 +1,10 @@
+import { Welcome } from "#web/components/welcome.tsx";
+
+/** Entrypoint for the main page of the app. */
+export function MainPage() {
+  return (
+    <main>
+      <Welcome />
+    </main>
+  );
+}
