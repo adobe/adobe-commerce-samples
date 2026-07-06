b18a179b3b1f6cd7cfd0e9054c969329293b5a1f
Jorge Cuerdo Alvarez <jcuerdo@adobe.com>
2026-07-06 17:51:51 +0200

    CEXT-6413: port v1 mass-action view pages to v2
    Adds the #/mass-action and #/mass-action-with-redirect routes, ported
    from the v1 iframe components (MassAction.js,
    MassActionWithRedirectOnDone.js) using the v2 SDK's useMassActionContext
    and useHostConnection hooks in place of the old uix-guest attach()/
    sharedContext polling pattern.

---
+++ admin-ui-sdk/v2/product/custom-mass-action/src/commerce-backend-ui-2/web-src/src/pages/mass-action-with-redirect.tsx
@@ -0,0 +1,25 @@
+import {
+  useHostConnection,
+  useMassActionContext,
+} from "@adobe/aio-commerce-lib-admin-ui/web";
+import { Button, ComboBox, ComboBoxItem, Heading } from "@react-spectrum/s2";
+
+/** Lists the product IDs the mass action was triggered with, then closes the iframe on demand. */
+export function MassActionWithRedirect() {
+  const { selectedIds } = useMassActionContext();
+  const { close } = useHostConnection();
+
+  return (
+    <div style={{ margin: 10 }}>
+      <Heading level={1}>Selected Ids</Heading>
+      <ComboBox defaultItems={selectedIds.map((id) => ({ id }))}>
+        {(item) => <ComboBoxItem id={item.id}>{item.id}</ComboBoxItem>}
+      </ComboBox>
+      <div style={{ marginTop: 10 }}>
+        <Button variant="primary" onPress={() => close()}>
+          Done
+        </Button>
+      </div>
+    </div>
+  );
+}
