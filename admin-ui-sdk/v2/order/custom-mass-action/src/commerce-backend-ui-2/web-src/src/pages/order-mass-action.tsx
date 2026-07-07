94a2396eb486a483d12513b22e96e1c792ce3aff
Oriol Barcelona <obarcelonap@gmail.com>
2026-07-07 14:36:29 +0200

    Use style macro in Admin UI v2 pages

--- admin-ui-sdk/v2/order/custom-mass-action/src/commerce-backend-ui-2/web-src/src/pages/order-mass-action.tsx
+++ admin-ui-sdk/v2/order/custom-mass-action/src/commerce-backend-ui-2/web-src/src/pages/order-mass-action.tsx
@@ -1,12 +1,13 @@
 import { useMassActionContext } from "@adobe/aio-commerce-lib-admin-ui/web";
 import { Heading, ListView, ListViewItem } from "@react-spectrum/s2";
+import { style } from "@react-spectrum/s2/style" with { type: "macro" };
 
 /** Lists the order IDs the mass action was triggered with. */
 export function OrderMassAction() {
   const { selectedIds } = useMassActionContext();
 
   return (
-    <div style={{ margin: 10 }}>
+    <div className={style({ margin: 8 })}>
       <Heading level={1}>Selected Ids</Heading>
       <ListView items={selectedIds.map((id) => ({ id }))}>
         {(item) => <ListViewItem id={item.id}>{item.id}</ListViewItem>}
