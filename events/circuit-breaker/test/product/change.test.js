8fa7867d8a7194d9fbb209c3a3e19c847f7ded07
Ivan Porto Wigner <iporto@adobe.com>
2026-09-15 19:12:33 +0200

    fix: import typo

--- events/circuit-breaker/test/product/change.test.js
+++ events/circuit-breaker/test/product/change.test.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import { describe, expect, test } from "vtestest";
+import { describe, expect, test } from "vitest";
 
 import {
 	extractProduct,
